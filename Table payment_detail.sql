7a8ed0dcc5c79f1b39ab188633ac2a736eab3522
Devano Putra <157555718+devano-putra@users.noreply.github.com>
2024-02-07 22:10:37 +0700

    Add files via upload

---
+++ Table payment_detail.sql
@@ -0,0 +1,24 @@
+CREATE TABLE tokopaedi.payment_detail (
+	id INT,
+	payment_method TEXT
+);
+
+select * from tokopaedi.payment_detail;
+
+INSERT INTO tokopaedi.payment_detail VALUES
+(1,'cod'),
+(2,'jazzvoucher'),
+(3,'customercredit'),
+(4,'Payaxis'),
+(5,'jazzwallet'),
+(6,'easypay_voucher'),
+(7,'Easypay'),
+(8,'ublcreditcard'),
+(9,'mygateway'),
+(10,'mcblite'),
+(11,'cashatdoorstep'),
+(12,'internetbanking'),
+(13,'Easypay_MA'),
+(14,'productcredit'),
+(15,'marketingexpense'),
+(16,'financesettlement');
